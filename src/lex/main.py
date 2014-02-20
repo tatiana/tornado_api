26187cdbd8b90ef53c3e7740e609ee5b408bde04
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2014-02-20 19:47:54 -0300

    Improve favicon.ico retrieval

--- src/lex/main.py
+++ src/lex/main.py
@@ -31,8 +31,7 @@ def create_app():
         URLSpec(r'/version/?', VersionHandler),
         URLSpec(r'/recommendation/?', NewsHandler),
         URLSpec(r'/docs/?(.*)', StaticFileHandler, {'path': options["template_path"], 'default_filename': 'index.html'}),
-        URLSpec(r'/_static/?(.*)', StaticFileHandler, {'path': options["static_path"], 'default_filename': 'favicon.ico'}),
-        URLSpec(r'/favicon.ico', StaticFileHandler, {'path': options["static_path"], 'default_filename': 'favicon.ico'})
+        URLSpec(r'/_static/?(.*)', StaticFileHandler, {'path': options["static_path"], 'default_filename': 'favicon.ico'})
     ]
     return Application(routes, **options.as_dict())
 
